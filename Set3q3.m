97b6deb8422b158855f9a9218a2fe1e7e31b0da1
khamkins16 <122921313+khamkins16@users.noreply.github.com>
2023-02-01 12:31:24 -0800

    Add files via upload

--- Set3q3.m
+++ Set3q3.m
@@ -81,11 +81,6 @@ sa2 = s;
 Sa2 = sa2*mdot_air;
 Ha2 = ha2*mdot_air;
 
-% Find the isentropic efficiency for this pressure ratio just for fun.
-%     set(gas,'S',sa1,'P',Pa2);
-%     ha2s = enthalpy_mass(gas);
-%     Air_Compressor_Isentropic_Efficiency = (ha2s - ha1)/(ha2 - ha1);
-
 % Start with fuel at ambient conditions.
 xfuel = zeros(1,N);
 xfuel(iCH4)  = 0.907;
@@ -144,11 +139,6 @@ end
 Tf2 = temperature(gas);
 hf2 = h;
 sf2 = s;
-
-% Find the isentropic efficiency for this pressure ratio just for fun.
-%     set(gas,'S',sf1,'P',Pf2);
-%     hf2s = enthalpy_mass(gas);
-%     Fuel_Compressor_Isentropic_Efficiency = (hf2s - hf1)/(hf2 - hf1)
 
 % Vary the fuel flow rate (at fixed air flow rate) to find the rate that
 % gives the correct turbine inlet temperature.
@@ -235,18 +225,20 @@ Pw7 = Pw6*Condenser_Pressure_Ratio;
 set(water, 'P', Pw6, 'Vapor', Vaporfrac_6w);
 hw6 = enthalpy_mass(water);
 Tw6 = temperature(water);
-Tw7 = Tw6;
-set(water, 'P', Pw7, 'T', Tw7);
+Xw7 = 0; % saturated liquid comes out of condenser
+set(water, 'P', Pw7, 'Vapor', Xw7);
 hw7 = enthalpy_mass(water);
 sw7 = entropy_mass(water);
+Tw7 = temperature(water);
 
 Pw8 = Po;
 [work78, hw8] = comp_work(Condense_Pump_Polytropic_Efficiency, Pw7, Pw8, hw7);
 set(water, 'H', hw8, 'P', Pw8);
 Tw8 = temperature(water);
 
-
-Pw2_guess = 63000000;
+% took out iteration loop to save run time here; this pressure was found
+% through iteration
+Pw2_guess = 10490000;
 Pw2 = Pw2_guess;
 
 [work12, hw2] = comp_work(Feed_Pump_Polytropic_Efficiency, Pw1, Pw2, hw1);
@@ -259,35 +251,92 @@ Pw3 = Econ_Pressure_Ratio*Pw2;
 Pw4 = Boiler_Pressure_Ratio*Pw3;
 Pw5 = Superheater_Pressure_Ratio*Pw4;
 
+VapFracw3 = 0;
+set(water,'P',Pw3,'Vapor',VapFracw3);
+hw3 = enthalpy_mass(water);
+Tw3 = temperature(water);
+
+VapFracw4 = 1;
+set(water,'T',Tw3,'Vapor',VapFracw4);
+hw4 = enthalpy_mass(water);
+Tw4 = temperature(water);
+
 Pm6 = Pm5;
 Pm7 = Pm6;
 Pm8 = Pm7;
 
 % Find heat transferred through HRSG
-set(gas, 'T',Tm5,'P',Pw5);
-hlc1 = enthalpy_mass(gas);
-set(gas,'T',Tw2,'P',Pw2);
-hlc2 = enthalpy_mass(gas);
+set(water, 'T',Tm5,'P',Pw5);
+hlc1 = enthalpy_mass(water);
+set(water,'T',Tw2,'P',Pw2);
+hlc2 = enthalpy_mass(water);
 qmax = hlc1 - hlc2;
 qactual = HRSG_effectiveness*qmax;
 
-hm8 = hm5 - qactual;
-set(gas,'H',hm8,'P',Pm8);
-Tm8 = temperature(gas);
+% hm8 = hm5 - qactual;
+% set(gas,'H',hm8,'P',Pm8,'X');
+% Tm8 = temperature(gas);
 
 hw5 = hw2 + qactual;
 set(water, 'H',hw5,'P',Pw5);
 Tw5 = temperature(water);
 
-% Pw5_guess = 49000000;
-% Tw5 = 750;
-% set(water,'T',Tw5,'P',Pw5_guess);
-% hw5 = enthalpy_mass(water);
-
 [work56, hw6hold, X6] = turb_work(Steam_Turbine_Polytropic_Efficiency, Pw5, Pw6, hw5);
 set(water, 'H',hw6hold,'P',Pw6);
-disp(X6);
+% disp(X6);
 
+% find mass flow rate through pinch point analysis
+mdot_water_guess = 20;
+mdot_water = mdot_water_guess;
+while true
+    
+    m57diff = (mdot_water*(hw5-hw3))/mdot_mix;
+    set(gas, 'P',Pm7,'H',hm5-m57diff);
+    Tm7 = temperature(gas);
+    pinch = Tm7-Tw3;
+    if pinch - Pinch_Point_Temp_Diff <= 0.0001
+        break
+    end
+    mdot_water = mdot_water + 0.001;
+end
+
+m56diff = mdot_water*(hw5-hw4)/mdot_mix;
+hm6 = hm5 - m56diff;
+set(gas,'P',Pm6,'H',hm6);
+Tm6 = temperature(gas);
+Hm6 = hm6*mdot_mix;
+
+hm7 = hm5 - m57diff;
+set(gas,'P',Pm7,'H',hm7);
+Tm7 = temperature(gas);
+Hm7 = hm7*mdot_mix;
+
+m78diff = mdot_water*(hw3-hw2)/mdot_mix;
+hm8 = hm7 - m78diff;
+set(gas,'P',Pm8,'H',hm8);
+Tm8 = temperature(gas);
+Hm8 = hm8*mdot_mix;
+
+Hw1 = mdot_water*hw1;
+Hw2 = mdot_water*hw2;
+Hw3 = mdot_water*hw3;
+Hw4 = mdot_water*hw4;
+Hw5 = mdot_water*hw5;
+Hw6 = mdot_water*hw6;
+Hw7 = mdot_water*hw7;
+Hw8 = mdot_water*hw8;
+
+% Calculate Power produced by each turbine
+W_gas_turb = mdot_mix*(hm4-hm5);
+W_fuel_comp = mdot_fuel*(hf2 - hf1);
+W_air_comp = mdot_air*(ha2 - ha1);
+W_gas_net = W_gas_turb - W_fuel_comp - W_air_comp;
+
+W_steam_turb = mdot_water*(hw5-hw6);
+W_feed_pump = mdot_water*(hw2-hw1);
+W_cond_pump = mdot_water*(hw8-hw7);
+W_steam_net = W_steam_turb - W_feed_pump - W_cond_pump;
+    
 offset = abs(Hm5);
 Ha1_off = Ha1 + offset;
 Ha2_off = Ha2 + offset;
@@ -296,11 +345,19 @@ Hf2_off = Hf2 + offset;
 Hm3_off = Hm3 + offset;
 Hm4_off = Hm4 + offset;
 Hm5_off = Hm5 + offset;
+Hm6_off = Hm6 + offset;
+Hm7_off = Hm7 + offset;
+Hm8_off = Hm8 + offset;
 
-% Find the isentropic efficiency for this pressure ratio just for fun.
-%     set(gas,'S',sm4,'P',Pm5);
-%     hm5s = enthalpy_mass(gas);
-%     Mix_Turbine_Isentropic_Efficiency = (hm4 - hm5)/(hm4 - hm5s);
+off_wat = abs(Hw5);
+Hw1_off = Hw1 + off_wat;
+Hw2_off = Hw2 + off_wat;
+Hw3_off = Hw3 + off_wat;
+Hw4_off = Hw4 + off_wat;
+Hw5_off = Hw5 + off_wat;
+Hw6_off = Hw6 + off_wat;
+Hw7_off = Hw7 + off_wat;
+Hw8_off = Hw8 + off_wat;
 
 % Assemble an array of state points.
 Tair  = [Ta1 Ta2];
@@ -311,19 +368,14 @@ Tfuel = [Tf1 Tf2];
 Pfuel = [Pf1 Pf2];
 Sfuel = [Sf1 Sf2];
 Hfuel_off = [Hf1_off Hf2_off];
-Tmix  = [Tm4 Tm5];
-Pmix  = [Pm4 Pm5];
-Smix  = [Sm4 Sm5];
-Hmix_off  = [Hm4_off Hm5_off];
+Tmix  = [Tm4 Tm5 Tm6 Tm7 Tm8];
+% Pmix  = [Pm4 Pm5];
+% Smix  = [Sm4 Sm5];
+Hmix_off  = [Hm4_off Hm5_off Hm6_off Hm7_off Hm8_off];
 
-% Find the various energy transfers.
-% Air_Fuel_Mass_Ratio = mdot_air/mdot_fuel;
-W_fuel_compressor   = mdot_fuel*(hf2 - hf1);
-W_air_compressor    = mdot_air*(ha2 - ha1);
-W_gross_gas_turbine = mdot_mix*(hm4 - hm5);
-W_net_gas_turbine   = W_gross_gas_turbine - W_fuel_compressor - W_air_compressor;
-efficiency_lhv     = W_net_gas_turbine/(mdot_fuel*LHV_fuel);
-air_specific_work   = W_net_gas_turbine/mdot_air;
+Twat = [Tw1 Tw2 Tw3 Tw4 Tw5 Tw6 Tw7 Tw8];
+Hwat_off = [Hw1_off Hw2_off Hw3_off Hw4_off Hw5_off Hw6_off Hw7_off Hw8_off];
+
 
 figure(1)
 plot(Hair_off/mdot_fuel/1e6,Tair,'bo--')
@@ -341,12 +393,88 @@ text(Hf2_off/mdot_fuel/1e6,Tf2,'f,2')
 text(Hm3_off/mdot_fuel/1e6,Tm3,'m,3')
 text(Hm4_off/mdot_fuel/1e6,Tm4,'m,4')
 text(Hm5_off/mdot_fuel/1e6,Tm5,'m,5')
-% text(30,1750,sprintf('Thermal Efficiency:  %.1f%% (LHV)',100*Eff_gas_turbine))
-% text(30,1550,sprintf('Air-Specific Work:   %.2f MJ/kg-air',Air_Specific_Work/1e6))
-% text(30,1350,sprintf('Fuel LHV:                 %.1f MJ/kg',LHV_fuel/1e6))
+text(Hm6_off/mdot_fuel/1e6,Tm6,'m,6')
+text(Hm7_off/mdot_fuel/1e6,Tm7,'m,7')
+text(Hm8_off/mdot_fuel/1e6,Tm8,'m,8')
 
-xlabel('Fuel-Specific Enthalpy Difference (MJ/kg_f_u_e_l-K)')
-ylabel('Temperature (K)')
+plot(Hwat_off/mdot_fuel/1e6,Twat,'mo--');
+text(Hw1_off/mdot_fuel/1e6,Tw1,'w,1');
+text(Hw2_off/mdot_fuel/1e6,Tw2,'w,2');
+text(Hw3_off/mdot_fuel/1e6,Tw3,'w,3');
+text(Hw4_off/mdot_fuel/1e6,Tw4,'w,4');
+text(Hw5_off/mdot_fuel/1e6,Tw5,'w,5');
+text(Hw6_off/mdot_fuel/1e6,Tw6,'w,6');
+text(Hw7_off/mdot_fuel/1e6,Tw7,'w,7');
+text(Hw8_off/mdot_fuel/1e6,Tw8,'w,8');
+
+Wg = round(W_gas_net/1e6,4,'significant');
+Ws = round(W_steam_net/1e6,4,'significant');
+text(-28,1500,strcat('Gas Turbine Power:  ', num2str(Wg), ' MW'),'FontWeight','bold');
+text(-28,1400,strcat('Steam Turbine Power:  ', num2str(Ws), ' MW'),'FontWeight','bold');
+
+xlabel('Fuel-Specific Enthalpy Difference (MJ/kg_f_u_e_l-K)');
+ylabel('Temperature (K)');
+title('Combined Cycle');
+
+% add vapor dome
+% Set up a fluid object to work with in Cantera.
+fluid = Water();
+
+% Get the critical point props.
+Tc = critTemperature(water);
+Pc = critPressure(water);
+set(water,'T',Tc,'P',Pc);
+sc = entropy_mass(water);
+uc = intEnergy_mass(water);
+hc = enthalpy_mass(water);
+% Set the triple point props.  Use a value epsilon above Tt to get the
+% bottom edge of the vapor dome.
+Tt = 273.165;
+Pt = satPressure(water,Tt);
+setState_Tsat(water,[Tt 0]);
+sft = entropy_mass(water);
+uft = intEnergy_mass(water);
+hft = enthalpy_mass(water);
+vft = 1/density(water);
+setState_Tsat(water,[Tt 1]);
+sgt = entropy_mass(water);
+ugt = intEnergy_mass(water);
+hgt = enthalpy_mass(water);
+vgt = 1/density(water);
+% Set the limits for data curves.
+Tmin = Tt;
+Tmax = 1100;
+Pmin = Pt;
+Pmax = 10000*1e5;
+% Make a vapor dome.
+dT = (Tc-Tmin)/150;
+i = 1;
+%setState_Tsat(water,[Tmin 0]);
+for T=Tmin:dT:Tc-dT    % Stop short of the critical point.
+    Tsatline(i) = T;
+    setState_Tsat(water,[T 0]);
+    sliqline(i) = entropy_mass(water);
+    uliqline(i) = intEnergy_mass(water);
+    hliqline(i) = enthalpy_mass(water);
+    setState_Tsat(water,[T 1]);
+    svapline(i) = entropy_mass(water);
+    uvapline(i) = intEnergy_mass(water);
+    hvapline(i) = enthalpy_mass(water);
+    i = i+1;
+end
+Tsatline(i) = Tc;   % Add the critical point now.
+sliqline(i) = sc;
+uliqline(i) = uc;
+hliqline(i) = hc;
+svapline(i) = sc;
+uvapline(i) = uc;
+hvapline(i) = hc;
+
+hliqline = (hliqline*mdot_water + off_wat)/mdot_fuel;
+hvapline = (hvapline*mdot_water + off_wat)/mdot_fuel;
+plot(hliqline/1e6,Tsatline,'k')
+plot(hvapline/1e6,Tsatline,'k')
+hold off;
 
 function [total_w, H] = comp_work(eta, Pin, Pout, h0)
     w = Water();
