98bb59174d47c572d7820b27e9f4bb6becfafeae
ahuyn012 <119711427+ahuyn012@users.noreply.github.com>
2023-02-21 20:38:54 -0800

    Add files via upload

--- Project 6/Saturation_iT.m
+++ Project 6/Saturation_iT.m
@@ -1,8 +1,8 @@
-function [P_sat,r_liq,r_vap] = Saturation_iT(T)
-%Saturation_iT returns the saturation pressure (Pa), saturated liquid
-%density, and saturated vapor density (kg/m^3) for given T (K) between the
-%triple temperature and near-critical temperature.
-    % Uses Maxwell's Equal-Area Rule
+function [sat_pres, r_sat_liq, r_sat_vap] = Saturation_iT(T)
+%Liquid_Spinodal_iT returns the location/density (kg/m^3) of the liquid
+%numerical spinodals (outermost locations at which dP/dr goes to zero) for
+%any given temperature (K).
+%Dongwon Ka
 
 % Provide access to support files via the Matlab path.
 addpath 'Fundamental_Relation_Files'
@@ -15,53 +15,48 @@ Setup_Props_i;
 
 % Set which of the loaded species you want to work with.  You might want to
 % change this around to see what the plots look like for other species.
-ispecies = nH2
+ispecies = nH2;
 
-% Setting the hyperparameters.
-m = 100; % number of samples
-m_elite = 10; % number of elite samples
-covar = eye(1); % covariance matrix
+% chemical potential min_spec_vol to liquid_spinodal point
+% Set limits and step size.
+rmax_1 = rupper_i(ispecies);
+rmin_1 = Liquid_Spinodal_iT(T); 
 
-% Starting point average.
-liq_spin = Liquid_Spinodal_iT(T);
-vap_spin = Vapor_Spinodal_iT(T);
-P1 = P_irT(ispecies,liq_spin,T)/1e6;
-P2 = P_irT(ispecies,vap_spin,T)/1e6;
-avg = (P1+P2)/2; % (MPa)
-
-dP_target = 0; % target area difference above/below P
-dP_best = 9999; % initialize variable > tolerance
-
-% Cross-entropy method.
-while abs((dP_best-dP_target)) > 0.1 % set tolerance
-   samples =  mvnrnd(avg,covar,m); % creates multivariate normal distribution
-   for s = 1:length(samples)
-       vmin = 1/rl_iTP(ispecies,T,samples(s)); %something is wrong here - I want to get the saturation v at specified P
-       vmax = 1/rv_iTP(ispecies,T,samples(s));
-       x = linspace(vmin,vmax,200);
-       p = 1;
-       for j = 1:length(x)
-           Pvisotherm(p) = P_irT(ispecies,1/x(j),T)/1e6;
-           p = p+1;
-       end
-       y = Pvisotherm - ones(1,200)*samples(s);
-       dP(s) = abs(trapz(x,y));
-   end
-   [out,idx] = sort(dP); % get index of sorted samples
-   elite_index = idx(1:m_elite); % retains the best m_elite samples' indices
-   elites = samples(elite_index); % lists elite samples
-   avg = mean(elites); % calculates new mean of elite samples
-   covar = cov(elites); % calculates new covariance matrix of elite samples
-   P_best = min(elites); % modification to cross-entropy method to get absolute minimum
-   vmin = 1/rl_iTP(ispecies,T,avg);
-   vmax = 1/rv_iTP(ispecies,T,avg);
-   x = linspace(vmin,vmax,200);
-   y = ones(1,200)*samples(s);
-   dP_best = trapz(x,y); % returns dP using the last mean value
+% Density range.
+pts      = 1000;
+r_list_1 = linspace(rmin_1,rmax_1,pts);
+for j=1:pts
+    p_list_1(j)  = P_irT(ispecies, r_list_1(j), T);
+    mu_list_1(j) = mu_irT(ispecies, r_list_1(j), T);
 end
 
-P_sat = P_best; % best P thus far (MPa)
-r_liq = rl_iTP(ispecies,T,P_sat); % liquid density (kg/m^3) at P_sat, T
-r_vap = rv_iTP(ispecies,T,P_sat); % vapor density (kg/m^3) at P_sat, T
+% chemical potential vapor_spinodal point to max_spec_vol
+rmax_2   = Vapor_Spinodal_iT(T);
+rmin_2   = rgtrip_i(ispecies);
 
+% Density range
+r_list_2 = linspace(rmin_2,rmax_2,pts);
+for j=1:pts
+    p_list_2(j)  = P_irT(ispecies, r_list_2(j), T);
+    mu_list_2(j) = mu_irT(ispecies, r_list_2(j), T);
 end
+
+% Find the cross-point
+test = 10000000;
+num_r_list_1 = 0;
+num_r_list_2 = 0;
+
+for i=1:pts
+    for j=1:pts
+        if sqrt((p_list_2(i)-p_list_1(j))^2+(mu_list_2(i)-mu_list_1(j))^2) < test
+            test = sqrt((p_list_2(i)-p_list_1(j))^2+(mu_list_2(i)-mu_list_1(j))^2);
+            num_r_list_1 = j;
+            num_r_list_2 = i;                
+        end
+    end
+end
+
+% output
+sat_pres  = P_irT(ispecies, r_list_1(num_r_list_1),T);
+r_sat_liq = r_list_1(num_r_list_1);
+r_sat_vap = r_list_2(num_r_list_2);
