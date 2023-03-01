ea6e8823d22277feab01d8fb0f43b93d5d249900
khamkins16 <122921313+khamkins16@users.noreply.github.com>
2023-03-01 14:39:27 -0800

    need to fix ideal functions to converge at low T

---
+++ Project 7/Project7Part2a.m
@@ -0,0 +1,94 @@
+% Project 7 Part 2a
+
+addpath 'Fundamental Relation Files'
+addpath 'Fundamental Relation Data'
+addpath 'Mixture Models'
+addpath 'Setup Files'
+addpath 'Property Files'
+addpath 'Procedure Files'
+
+clear all
+format compact
+fprintf('\n************************************************************\n')
+
+% Set up the basic storage and load the FR files and mixture model.
+% Set the number of components in mixture (N = 2 for binary, N = 3 for ternary).
+N = 3;
+Setup_Air_Props;
+
+% Set fixed-point values that are specific to air.  See Lemmon et al. 2000.
+% The composition is: N2:O2:Ar = 0.7812:0.2096:0.0092
+% Be sure to change these as needed if you use engineering air (79:21).
+% And ignore these if you use an arbitrary composition.  They are for air.
+Mair = 28.9586;                 % kg/kmol
+Tmaxcondentherm = 132.6312;     % K
+Pmaxcondentherm = 3.78502e6;    % Pa
+rmaxcondentherm = 10.4477*Mair; % kg/m3
+Tmaxcondenbar   = 132.6035;     % K
+Pmaxcondenbar   = 3.7891e6;     % Pa
+rmaxcondenbar   = 11.0948*Mair; % kg/m3
+Tcritair        = 132.5306;     % K
+Pcritair        = 3.7860e6;     % Pa
+rcritair        = 11.8308*Mair; % kg/m3
+% Bottom of dome conditions for air:
+Tsolidair       = 59.75;        % K
+Psolidair       = 5265;         % Pa
+rsolidair       = 33.067*Mair;  % kg/m3
+% Upper limit conditions for the model:
+Tupper          = 870;          % K
+Pupper          = 100e6;        % Pa
+rupper          = rsolidair;
+% Lower limit to stay just above solid air:
+Tlower          = 60;           % K
+% Molar masses
+MW_O2 = 31.999; % (g/mol)
+MW_N2 = 28.0134; % (g/mol)
+MW_Ar = 39.948; % (g/mol)
+
+% Set an air composition.
+c = zeros(N,1);
+if(N == 3)
+    % Use ternary air.
+    c(O2) = 0.2096;
+    c(Ar) = 0.0092;
+    c(N2) = 1 - c(O2) - c(Ar);
+end
+if(N == 2)
+    % Use binary air.
+    c(O2) = 0.21;
+    c(N2) = 1 - c(O2);
+end
+
+% Get the inflection point for this composition.
+[Tinfl, rinfl] = Pr_Inflection_c(c);
+Pinfl = P_crT(c,rinfl,Tinfl);
+
+
+Temps = linspace(74,Tinfl,10);
+
+for i = 1:length(Temps)
+    T = Temps(i);
+    
+    [p,rg,rf,x] = Dew_cT(c,T);
+    P_dew(i) = p;
+    rho_dew(i) = rg;
+    rho_dew_comp(i) = rf;
+    XN2_dew(i) = x(N2);
+    XO2_dew(i) = x(O2);
+    XAr_dew(i) = x(Ar);
+    
+    [p,rf,rg,x] = Bubble_cT(c,T);
+    P_bub(i) = p;
+    rho_bub(i) = rf;
+    rho_bub_comp(i) = rg;
+    XN2_bub(i) = x(N2);
+    XO2_bub(i) = x(O2);
+    XAr_bub(i) = x(Ar);
+end
+
+figure(1)
+plot(rho_dew,Temps,'o');
+hold on;
+plot(rho_dew_comp,Temps,'o');
+plot(rho_bub,Temps,'o');
+plot(rho_bub_comp,'o');
