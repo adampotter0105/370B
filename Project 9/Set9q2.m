792107b2b7eba2a73d804e32b6a21c5f1d6e7e04
khamkins16 <122921313+khamkins16@users.noreply.github.com>
2023-03-14 13:08:52 -0700

    diffusivity + ionic conductivity

--- Project 9/Set9q2.m
+++ Project 9/Set9q2.m
@@ -13,6 +13,10 @@ b_np = 1.823;
 a_w = 3.64*10^-4; % for water with a nonpolar gas
 b_w = 2.334;
 
+K_0 = 15; % ionic conductivity at 1000C (S/m)
+E_a = 96500; % activation energy (J/mol)
+T_0 = 1000+273.15; % K
+
 % critical points
 Tc_H2 = 33.18; % K
 Pc_H2 = 13*100000/101325; % atm
@@ -29,8 +33,11 @@ M_N2 = .0280134; % kg/mol
 
 % temperature range
 T = linspace(600+273.15,1300+273.15); % K
-D_N2O2 = zeros(1, length(T));
+
+% results arrays
+D_N2O2 = zeros(1, length(T)); 
 D_H2H2O = zeros(1,length(T));
+K = zeros(1, length(T));
 
 P = 100000; % Pa
 P_atm = P/101325; % atm
@@ -46,12 +53,15 @@ for i = 1:length(T)
     set(anode,'T',T(i));
     
     % Bird Stewart Lightfoot equation (P: atm, T: K, D: cm2/s)
-    
     % cathode (N2, O2)
     D_N2O2(i) = ((a_np * (T(i)/sqrt(Tc_N2*Tc_O2))^b_np) * (Pc_N2*Pc_O2)^(1/3) * (Tc_N2*Tc_O2)^(5/12) * (1/M_N2 + 1/M_O2)^(1/2))/P_atm;
-    
     % anode (H2, H2O)
     D_H2H2O(i) = ((a_w * (T(i)/sqrt(Tc_H2*Tc_H2O))^b_w) * (Pc_H2*Pc_H2O)^(1/3) * (Tc_H2*Tc_H2O)^(5/12) * (1/M_H2 + 1/M_H2O)^(1/2))/P_atm;
+    
+    % ohmic losses
+    K(i) = K_0*exp(-E_a/R*(1/T(i) - 1/T_0));
+    
+    
     
 end
 
@@ -59,10 +69,10 @@ end
 D_N2O2_1000 = ((a_np * (1273/sqrt(Tc_N2*Tc_O2))^b_np) * (Pc_N2*Pc_O2)^(1/3) * (Tc_N2*Tc_O2)^(5/12) * (1/M_N2 + 1/M_O2)^(1/2))/P_atm;
 D_H2H2O_1000 = ((a_w * (1273/sqrt(Tc_H2*Tc_H2O))^b_w) * (Pc_H2*Pc_H2O)^(1/3) * (Tc_H2*Tc_H2O)^(5/12) * (1/M_H2 + 1/M_H2O)^(1/2))/P_atm;
 
-
 % normalize
 D_N2O2 = D_N2O2./D_N2O2_1000;
 D_H2H2O = D_H2H2O./D_H2H2O_1000;
+K = K./K_0;
 
 % plotting
 T_plot = T - 273.15;
@@ -71,7 +81,8 @@ figure(1)
 plot(T_plot,D_N2O2);
 hold on;
 plot(T_plot,D_H2H2O);
-legend('D_{N_2O_2}','D_{H_2H_2O}');
+plot(T_plot,K);
+legend('D_{N_2O_2}','D_{H_2H_2O}','Ionic Conductivity');
 xlabel(['Temperature (' char(176) 'C)']);
 ylabel('Normalized Quantities');
 hold off;
