7d76c95febd1fe03524a7fd67791cdf6a21503f8
khamkins16 <122921313+khamkins16@users.noreply.github.com>
2023-03-14 13:22:19 -0700

    Add files via upload

--- Project 9/Set9q2.m
+++ Project 9/Set9q2.m
@@ -17,6 +17,10 @@ K_0 = 15; % ionic conductivity at 1000C (S/m)
 E_a = 96500; % activation energy (J/mol)
 T_0 = 1000+273.15; % K
 
+Ea_ecd = 100000; % activation energy for cathode exchange current density (J/mol)
+ECD_cat_0 = 1000; % cathode exchange current density at 1000C (A/m2)
+ECD_an_0 = ECD_cat_0*100;
+
 % critical points
 Tc_H2 = 33.18; % K
 Pc_H2 = 13*100000/101325; % atm
@@ -38,6 +42,8 @@ T = linspace(600+273.15,1300+273.15); % K
 D_N2O2 = zeros(1, length(T)); 
 D_H2H2O = zeros(1,length(T));
 K = zeros(1, length(T));
+ECD_cat = zeros(1,length(T));
+ECD_an = zeros(1,length(T));
 
 P = 100000; % Pa
 P_atm = P/101325; % atm
@@ -58,9 +64,12 @@ for i = 1:length(T)
     % anode (H2, H2O)
     D_H2H2O(i) = ((a_w * (T(i)/sqrt(Tc_H2*Tc_H2O))^b_w) * (Pc_H2*Pc_H2O)^(1/3) * (Tc_H2*Tc_H2O)^(5/12) * (1/M_H2 + 1/M_H2O)^(1/2))/P_atm;
     
-    % ohmic losses
+    % ionic conductivity
     K(i) = K_0*exp(-E_a/R*(1/T(i) - 1/T_0));
     
+    % exchange current density
+    ECD_cat(i) = ECD_cat_0*exp(-Ea_ecd/R*(1/T(i) - 1/T_0));
+    ECD_an(i) = ECD_an_0*exp(-Ea_ecd/R*(1/T(i) - 1/T_0));
     
     
 end
@@ -73,6 +82,8 @@ D_H2H2O_1000 = ((a_w * (1273/sqrt(Tc_H2*Tc_H2O))^b_w) * (Pc_H2*Pc_H2O)^(1/3) * (
 D_N2O2 = D_N2O2./D_N2O2_1000;
 D_H2H2O = D_H2H2O./D_H2H2O_1000;
 K = K./K_0;
+ECD_cat = ECD_cat./ECD_cat_0;
+ECD_an = ECD_an./ECD_an_0;
 
 % plotting
 T_plot = T - 273.15;
@@ -82,7 +93,9 @@ plot(T_plot,D_N2O2);
 hold on;
 plot(T_plot,D_H2H2O);
 plot(T_plot,K);
-legend('D_{N_2O_2}','D_{H_2H_2O}','Ionic Conductivity');
+plot(T_plot,ECD_cat);
+% plot(T_plot,ECD_an);
+legend('D_{N_2O_2}','D_{H_2H_2O}','Ionic Conductivity','Cathode Exchange Current Density');
 xlabel(['Temperature (' char(176) 'C)']);
 ylabel('Normalized Quantities');
 hold off;
